795c518f31ee45436a229e56b413d1913c872ee1
Igor Souto <igor_souto@outlook.com>
2021-05-12 10:37:43 -0300

    test(header): add tests to header
    Add tests to header.

---
+++ components/common/__tests__/HeaderNavigation.spec.ts
@@ -0,0 +1,43 @@
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import HeaderNavigation from '@/components/common/HeaderNavigation.vue'
+import { siteMap } from '@/utils/siteMap'
+
+const mountComponent = () =>
+  mount(HeaderNavigation, {
+    stubs: {
+      NuxtLink: RouterLinkStub,
+    },
+  })
+
+describe('HeaderNavigation', () => {
+  it('should mount component', () => {
+    const wrapper = mountComponent()
+
+    expect(wrapper.vm).toBeDefined()
+  })
+
+  it('should display links for all siteMap', () => {
+    const wrapper = mountComponent()
+
+    const siteMapList = Object.values(siteMap)
+    const links = wrapper.findAllComponents(RouterLinkStub)
+    const buttons = wrapper.findAll('li')
+
+    expect(buttons.length).toBe(siteMapList.length)
+
+    siteMapList.forEach(({ url, label }, index) => {
+      expect(links.at(index).props().to).toBe(url)
+      expect(wrapper.text()).toContain(label)
+    })
+  })
+
+  it('should emit "navigate" on link click', async () => {
+    const wrapper = mountComponent()
+
+    const link = wrapper.findAll('li').at(0)
+
+    await link.trigger('click')
+
+    expect(wrapper.emitted('navigate')).toBeTruthy()
+  })
+})
